5f94e872d3fad73ddf89e3178caf7c093ce03858
leonardo meitz <leonardomeitz@gmail.com>
2018-03-21 19:31:54 +0100

    Android.mk: delete TimeService apk

--- Android.mk
+++ Android.mk
@@ -18,15 +18,5 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),s3ve3g)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := TimeService
-LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := proprietary/app//.apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_CLASS := APPS
-LOCAL_CERTIFICATE := platform
-include $(BUILD_PREBUILT)
-
 endif
 
