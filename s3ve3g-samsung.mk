4fbae2ac8c57ea8bf8e93a8be7db2ecea292c3f3
Francescodario Cuzzocrea <bosconovic@gmail.com>
2019-05-07 23:30:55 +0200

    s3ve3g: add common makefile for common blobs and cameras makefiles

---
+++ s3ve3g-samsung.mk
@@ -0,0 +1,29 @@
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_COPY_FILES += \
+	vendor/samsung/s3ve3g/samsung/vendor/lib/B08QT_libTsAe.so:system/vendor/lib/B08QT_libTsAe.so \
+	vendor/samsung/s3ve3g/samsung/vendor/lib/B08QT_libTsAf.so.so:system/vendor/lib/B08QT_libTsAf.so \
+	vendor/samsung/s3ve3g/samsung/vendor/lib/B08QT_libTs_J_Accm.so:system/vendor/lib/B08QT_libTs_J_Accm.so \
+	vendor/samsung/s3ve3g/samsung/vendor/lib/B08QT_libTs_J_Awb.so:system/vendor/lib/B08QT_libTs_J_Awb.so \
+	vendor/samsung/s3ve3g/samsung/vendor/lib/libmmcamera2_stats_algorithm.so:system/vendor/lib/libmmcamera2_stats_algorithm.so \
+	vendor/samsung/s3ve3g/samsung/vendor/lib/libmmcamera_fidelix_eeprom.so:system/vendor/lib/libmmcamera_fidelix_eeprom.so \
+	vendor/samsung/s3ve3g/samsung/vendor/lib/libmmcamera2_iface_modules.so:system/vendor/lib/libmmcamera2_iface_modules.so \
+	vendor/samsung/s3ve3g/samsung/vendor/lib/libmmcamera2_imglib_modules.so:system/vendor/lib/libmmcamera2_imglib_modules.so \
+	vendor/samsung/s3ve3g/samsung/vendor/lib/libmmcamera2_isp_modules.so:system/vendor/lib/libmmcamera2_isp_modules.so \
+	vendor/samsung/s3ve3g/samsung/vendor/lib/libmmcamera2_sensor_modules.so:system/vendor/lib/libmmcamera2_sensor_modules.so \
+	vendor/samsung/s3ve3g/samsung/vendor/lib/libmmcamera2_stats_modules.so:system/vendor/lib/libmmcamera2_stats_modules.so \
+	vendor/samsung/s3ve3g/samsung/vendor/lib/libmmcamera_imx175.so:system/vendor/lib/libmmcamera_imx175.so \
+	vendor/samsung/s3ve3g/samsung/vendor/lib/libmmcamera_s5k4h5yb.so:system/vendor/lib/libmmcamera_s5k4h5yb.so \
+	vendor/samsung/s3ve3g/samsung/vendor/lib/liboemcamera.so:system/vendor/lib/liboemcamera.so
