4572c7d648ef977b3858858dfcd5d18f699db8d4
PythonLimited <leonardomeitz@gmail.com>
2019-07-21 22:56:18 +0200

    remove double .so ending

--- s3ve3g-samsung-camera-vendor.mk
+++ s3ve3g-samsung-camera-vendor.mk
@@ -14,7 +14,7 @@
 
 PRODUCT_COPY_FILES += \
 	vendor/samsung/s3ve3g/samsung/vendor/lib/E08QL_libTsAe.so:system/vendor/lib/E08QL_libTsAe.so \
-	vendor/samsung/s3ve3g/samsung/vendor/lib/E08QL_libTsAf.so.so:system/vendor/lib/E08QL_libTsAf.so \
+	vendor/samsung/s3ve3g/samsung/vendor/lib/E08QL_libTsAf.so:system/vendor/lib/E08QL_libTsAf.so \
 	vendor/samsung/s3ve3g/samsung/vendor/lib/E08QL_libTs_J_Accm.so:system/vendor/lib/E08QL_libTs_J_Accm.so \
 	vendor/samsung/s3ve3g/samsung/vendor/lib/E08QL_libTs_J_Awb.so:system/vendor/lib/E08QL_libTs_J_Awb.so \
 	vendor/samsung/s3ve3g/samsung/vendor/lib/libmmcamera2_stats_algorithm.so:system/vendor/lib/libmmcamera2_stats_algorithm.so \
